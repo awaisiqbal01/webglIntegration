4e18e2b11d658b6c8e21a6ca8a7f5d667edb2cc3
awaisiqbal01 <awais@deaftawk.com>
2025-02-02 18:44:04 +0100

    allowed all origins

--- api/server.js
+++ api/server.js
@@ -2,10 +2,19 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+const cors = require("cors");
 const PORT = 2025;
 
 app.use(express.static(path.join(__dirname, "../public/")));
 app.use("/Build", express.static(path.join(__dirname, "../public/Build"))); 
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, ".../public/index.html"));
